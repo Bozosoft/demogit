46847f9be7f38c197cb699558d4e4a622f6eeacf
Bozosoft <jc.etiemble@gmail.com>
2013-01-29 20:58:30 +0100

    Modif 1

--- lib/class_session.php
+++ lib/class_session.php
@@ -10,7 +10,7 @@
  * @author : JC Etiemble - http://jc.etiemble.free.fr
  * @version :  2013
  * @copyright 2007-2013  (c) JC Etiemble
- * @package   GestAssoPhp+Pg
+ * @package   Test_demogig
  */
  
 /**
